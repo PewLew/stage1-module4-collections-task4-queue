8875d368dcbcba63a3a75d4876d576dc47a78276
Amachuarori <52317097+Amachuarori@users.noreply.github.com>
2024-01-15 21:08:13 +0600

    sfx

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,9 +1,19 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
-        return null;
+        List<Integer> dishes = new ArrayList<>();
+        int sum = 0;
+        while(dishes.size()!=numberOfDishes){
+            sum += everyDishNumberToEat;
+            if(sum >10)
+                sum = sum-10;
+            System.out.print(sum + " ");
+            dishes.add(sum);
+        }
+        return dishes;
     }
 }
